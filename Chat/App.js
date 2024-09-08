54a2ffa4fbab0dab09f93bf5df36884cb4a979e6
Sanitha S <sanithasudevan@gmail.com>
2024-09-09 00:05:28 +0530

    chat

--- Chat/App.js
+++ Chat/App.js
@@ -1,23 +1,24 @@
 
-const express=require("express");
-const app=express();
-const bodyParse=require("body-parser");
+const express = require( "express" );
+const app = express();
+const bodyParse = require( "body-parser" );
 
 
-const loginRoutes=require("./router/login.router")
-const chatRoutes=require("./router/chat.router")
+const loginRoutes = require( "./router/login.router")
+const chatRoutes = require( "./router/chat.router" )
 
-app.use(bodyParse.urlencoded({extended:true}));
+app.use( bodyParse.urlencoded( { extended: true } ) );
 
-app.use(loginRoutes);
-app.use(chatRoutes);
-
-
+app.use( loginRoutes );
+app.use( chatRoutes );
 
 
- app.use((req,res,next)=>{
 
-    res.send("<h1>Page Not Found 404</h1>")
 
-}) 
-app.listen(3001)
+app.use( ( req, res, next ) =>
+{
+
+    res.send( "<h1>Page Not Found 404</h1>" )
+
+} )
+app.listen( 3001 )
--- Chat/router/chat.router.js
+++ Chat/router/chat.router.js
@@ -1,14 +1,44 @@
 const express = require( "express" );
 const router = express.Router();
 const path = require( 'path' );
+const fs = require( "fs" );
+
+
+router.get( "/", ( req, res, next ) =>
+{
+    console.log( "From login page" )
+    res.sendFile( path.join( __dirname, "..", "views", "chat.html" ) )
+
+} )
 
 
 router.post( "/", ( req, res, next ) =>
+{
+    console.log( "from chat page:", req.body )
+
+    console.log( "uname:", req.body.username );
+    console.log( "message:", req.body.message );
+    const fileData = req.body.username + ':' + req.body.message + '\n';
+    fs.appendFile( "chat.txt", fileData, ( err ) =>
     {
-        console.log( "chat page:",req.body )
-        localStorage.setItem("username", req.body)
-        
-        res.sendFile( path.join( __dirname, "..", "views", "chat.html" ) )
+        if ( err )
+        {
+            console.log( "error in writing async file" );
+        }
+        else
+        {
+            fs.readFile( 'chat.txt', ( err, data ) =>
+            {
+                if ( err ) throw err;
+                console.log( data.toString() );
+                res.sendFile( path.join( __dirname, "..", "views", "chat.html" ) )
+            } );
+        }
     } )
+
+
+} )
+
+
 
 module.exports = router;
